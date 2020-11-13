87f623007ae5978cef17f5677b8eff2695b6b5da
0xbahaa <27164837+0xbahaa@users.noreply.github.com>
2020-11-13 05:51:13 -0800

    (feature) Make Heap array public not private

--- core/heap.go
+++ core/heap.go
@@ -13,7 +13,7 @@ type HeapEntry struct {
 
 type HeapManager struct {
 	base uint64
-	heap []*HeapEntry
+	Heap []*HeapEntry
 }
 
 func NewHeap(base uint64) *HeapManager {
@@ -26,9 +26,9 @@ func NewHeap(base uint64) *HeapManager {
 // Size returns the size of a particular heap entry, returns 0 of heap entry is not found
 func (self *HeapManager) Size(addr uint64) uint64 {
 	size := uint64(0)
-	for i := 0; i < len(self.heap); i++ {
-		if self.heap[i].Address == addr {
-			size = self.heap[i].Size
+	for i := 0; i < len(self.Heap); i++ {
+		if self.Heap[i].Address == addr {
+			size = self.Heap[i].Size
 			break
 		}
 	}
@@ -38,14 +38,14 @@ func (self *HeapManager) Size(addr uint64) uint64 {
 //Scans for a particular heap allocated with Malloc. Returns the index where
 //this heap exists in the heap data structure
 func (self *HeapManager) scan(size uint64) int {
-	heaplen := len(self.heap)
+	heaplen := len(self.Heap)
 
 	if heaplen == 0 {
 		return -1
 	}
 
-	for i, entry := range self.heap[:heaplen-1] {
-		nextEntry := self.heap[i+1]
+	for i, entry := range self.Heap[:heaplen-1] {
+		nextEntry := self.Heap[i+1]
 		gap := nextEntry.Address - (entry.Address + entry.Size)
 		if gap >= size+0x20 {
 			return i + 1
@@ -55,19 +55,19 @@ func (self *HeapManager) scan(size uint64) int {
 }
 
 func (self *HeapManager) nextAddress() uint64 {
-	heaplen := len(self.heap)
-	return self.heap[heaplen-1].Address + self.heap[heaplen-1].Size + 0x10
+	heaplen := len(self.Heap)
+	return self.Heap[heaplen-1].Address + self.Heap[heaplen-1].Size + 0x10
 }
 
 func (self *HeapManager) naiveMalloc(size uint64) uint64 {
-	heaplen := len(self.heap)
+	heaplen := len(self.Heap)
 
 	if heaplen == 0 {
-		self.heap = append(self.heap, &HeapEntry{self.base, size})
+		self.Heap = append(self.Heap, &HeapEntry{self.base, size})
 		return self.base
 	} else {
 		nxt := self.nextAddress()
-		self.heap = append(self.heap, &HeapEntry{nxt, size})
+		self.Heap = append(self.Heap, &HeapEntry{nxt, size})
 		return nxt
 	}
 }
@@ -75,9 +75,9 @@ func (self *HeapManager) naiveMalloc(size uint64) uint64 {
 // insert without looking
 func (self *HeapManager) insertHeap(index int, addr, size uint64) {
 	insertEntry := &HeapEntry{addr, size}
-	self.heap = append(self.heap, &HeapEntry{0, 0})
-	copy(self.heap[index+1:], self.heap[index:])
-	self.heap[index] = insertEntry
+	self.Heap = append(self.Heap, &HeapEntry{0, 0})
+	copy(self.Heap[index+1:], self.Heap[index:])
+	self.Heap[index] = insertEntry
 }
 
 func (self *HeapManager) Malloc(size uint64) uint64 {
@@ -85,7 +85,7 @@ func (self *HeapManager) Malloc(size uint64) uint64 {
 	if index == -1 {
 		return self.naiveMalloc(size)
 	}
-	nxt := self.heap[index-1].Address + self.heap[index-1].Size + 0x10
+	nxt := self.Heap[index-1].Address + self.Heap[index-1].Size + 0x10
 	self.insertHeap(index, nxt, size)
 	return nxt
 }
@@ -98,7 +98,7 @@ func (self *HeapManager) preferEndMalloc(align, size uint64) uint64 {
 	// if align is a power of 2, then we could use util.RoundUp(nxt, align-1)
 	// since this function is more generic, we can't do that
 	nxt += (align - (nxt % align)) % align
-	self.heap = append(self.heap, &HeapEntry{nxt, size})
+	self.Heap = append(self.Heap, &HeapEntry{nxt, size})
 	return nxt
 }
 
@@ -114,11 +114,11 @@ func (self *HeapManager) preferMalloc(addr, align, size uint64) uint64 {
 	// round up the address (if align is a power of 2, then we could use util.RoundUp)
 	addr += (align - (addr % align)) % align
 
-	heaplen := len(self.heap)
+	heaplen := len(self.Heap)
 
 	// look through the heaps
-	for i, heap := range self.heap[:heaplen-1] {
-		heapNext := self.heap[i+1]
+	for i, heap := range self.Heap[:heaplen-1] {
+		heapNext := self.Heap[i+1]
 		for {
 			// if the preferred address (+size) is bigger than the next heap
 			if addr+size >= heapNext.Address {
@@ -141,9 +141,9 @@ func (self *HeapManager) preferMalloc(addr, align, size uint64) uint64 {
 
 // returns a given heap block given some address
 func (self *HeapManager) getHeapBlock(addr uint64) (*HeapEntry, error) {
-	for i := 0; i < len(self.heap); i++ {
-		if self.heap[i].Address == addr {
-			return self.heap[i], nil
+	for i := 0; i < len(self.Heap); i++ {
+		if self.Heap[i].Address == addr {
+			return self.Heap[i], nil
 		}
 	}
 	return nil, fmt.Errorf("allocated region not found: 0x%x", addr)
@@ -168,9 +168,9 @@ func (self *HeapManager) ReAlloc(addr uint64, newsize uint64) (uint64, uint64) {
 }
 
 func (self *HeapManager) Free(addr uint64) uint64 {
-	for index, element := range self.heap {
+	for index, element := range self.Heap {
 		if element.Address == addr {
-			self.heap = append(self.heap[:index], self.heap[index+1:]...)
+			self.Heap = append(self.Heap[:index], self.Heap[index+1:]...)
 			return 1
 		}
 	}
